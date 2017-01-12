54f6b60e46d46dd57b83193639da875c3afb0405
Bunny303 <303bunnies@gmail.com>
2017-01-12 11:35:33 +0200

    - added example of picture recognition;

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,6 +1,10 @@
 let OrdersController = {
     create: (req, res) => {
-        res.render('order-create');
+        res.send({status: 1});
+    },
+
+    prepare: (req, res) => {
+        res.render('order-prepare');
     }
 };
 module.exports = OrdersController;
--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ const controllers = require('./controllers/base');
 let Routes = (app) => {
 
     app.get('/', controllers.home.index);
-    app.get('/order', controllers.orders.create);
+    app.get('/order-create', controllers.orders.create);
+    app.get('/order', controllers.orders.prepare);
 
     app.all('*', (req, res) => {
         res.status(404);
