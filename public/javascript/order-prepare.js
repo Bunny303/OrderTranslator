00f31187f4cc5c6a1584deb04b2635b9b59dcd41
Bunny303 <303bunnies@gmail.com>
2017-01-25 16:17:47 +0200

    - fixed counting of words in text document;

--- public/javascript/order-prepare.js
+++ public/javascript/order-prepare.js
@@ -1,6 +1,6 @@
-var wordsCount = function () {
-    if (this.files) {
-        var file = this.files[0];
+var wordsCount = function (evt) {
+    if (evt.files) {
+        var file = evt.files[0];
         if (file) {
             var resultText;
             //If uploaded file is picture, we will try to recognize the text inside
@@ -9,6 +9,8 @@ var wordsCount = function () {
                 Tesseract.recognize(file)
                     .then(function (result) {
                         resultText = result.text;
+                        //calculate number of words
+                        wordsNumber = resultText.split(" ").length;
                     })
                     .catch(function (err) {
                         console.error(err);
@@ -18,16 +20,21 @@ var wordsCount = function () {
                 //if uploaded file is document (doc, txt, odt and etc.)
                 //todo: not supported by all browsers
                 //todo: test with a very big file
-                var r = new FileReader();
-                r.onload = function (e) {
-                    var resultText = e.target.result;
-                }
-                r.readAsText(file);
-            }
+                var reader = new FileReader();
+                reader.onload = function (event) {
+                    resultText = event.target.result;
+                    //calculate number of words
+                    wordsNumber = resultText.split(" ").length;
+                    console.log(wordsNumber);
+                    update();
+                };
+                reader.onerror = function (event) {
+                    console.log(event);
+                    resultText = '';
+                };
 
-            //calculate number of words
-            var wordsNumber = resultText.split(" ").length;
-            console.log(wordsNumber);
+                reader.readAsText(file);
+            }
 
             //PDF
             /*PDFJS.getDocument( file ).then( function(pdf) {
@@ -38,3 +45,14 @@ var wordsCount = function () {
         }
     }
 };
+//
+//function update() {
+//    var req = new XMLHttpRequest();
+//    req.open("GET", "/order");
+//    req.onreadystatechange = function () {
+//        if (req.readyState == 4) {
+//            document.getElementById("content").innerHTML = req.responseText;
+//        }
+//    }
+//    req.send();
+//}
