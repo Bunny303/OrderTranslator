d8e245323788d33a9b38cca2854e32f8b230630b
Bunny303 <303bunnies@gmail.com>
2017-03-26 21:58:14 +0300

    fixed redis to go

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ let app = express();
 let env = process.env.NODE_ENV || 'development';
 let config = require('./config/config')[env];
 
-const redis = require('redis').createClient();
+let redis;
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    redis = require('redis').createClient();
+}
 const RedisStore = require('connect-redis')(session);
 
 app.set('port', config.port);
